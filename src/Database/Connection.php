b371e7f559fc9649748c2c57c5da8c2d9ca1af1e
Matej <matej.hlavac@student.ukf.sk>
2026-03-25 23:25:14 +0100

    Update Connection class: fixed typo miss

--- src/Database/Connection.php
+++ src/Database/Connection.php
@@ -17,7 +17,7 @@ class Connection {
 
         $options = [
             PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION, 
-            PDO::ATTR_DEFSULT_FETCH_MODE    => PDO::FETCH_ASSOC, 
+            PDO::ATTR_DEFAULT_FETCH_MODE    => PDO::FETCH_ASSOC, 
             PDO::ATTR_EMULATE_PREPARES      => false
         ];
 
