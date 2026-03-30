b250619f8c4c79ea47c2f2364434ad3a182987e6
Matej <matej.hlavac@student.ukf.sk>
2026-03-30 22:54:25 +0200

    Small tweaks in Track properties

--- src/Models/Track.php
+++ src/Models/Track.php
@@ -5,18 +5,18 @@ namespace App\Models;
 class Track {
     public $id;
     public $title;
+    public $genre;
     public $bpm;
-    public $key_signature;
     public $file_path;
     public $created_at;
 
 
-    public function __construct($title = null, $bpm = null, $key_signature = null, $file_path = null){
+    public function __construct($title = null, $genre = null, $bpm = null, $file_path = null){
         $this->title = $title;
+        $this->genre = $genre;
         $this->bpm = $bpm;
-        $this->key_signature = $key_signature;
         $this->file_path = $file_path;
     }
 
-    
+
 }
