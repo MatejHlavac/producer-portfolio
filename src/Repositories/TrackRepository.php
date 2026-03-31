561f61702144432d4daed1e0775ebc94424b2388
Matej <matej.hlavac@student.ukf.sk>
2026-03-31 21:04:45 +0200

    Create method findAll that returns array which contains all rows from the tracks table. Each element of array is individual instance of Track class.

--- src/Repositories/TrackRepository.php
+++ src/Repositories/TrackRepository.php
@@ -1,11 +1,20 @@
 <?php
 
 namespace App\Repositories;
+namespace App\Models\Track;
 
 class TrackRepository {
     private $dbConnection;
 
     public function __construct($db) {
         $this->dbConnection = $db;
+    }
+
+    public function findAll(){
+        $sql = "SELECT * FROM tracks";
+        $stmt = $this->dbConnection->prepare($sql);
+        $stmt->execute();
+
+
     }
 }
